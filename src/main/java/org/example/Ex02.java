06f716220552f19c0c078c19c70815cde7093b59
Douglas Liebl <112643737+DouglasLiebl@users.noreply.github.com>
2023-09-15 21:36:39 -0300

    Add files via upload

---
+++ src/main/java/org/example/Ex02.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ex02 {
+
+    private static void averageCalculator(Scanner input) {
+        double average = 0, count = 0;
+        try {
+            while(true) {
+                System.out.println("Enter a number:");
+                double n = input.nextDouble();
+
+                if(n < 0) {
+                    average /= count;
+                    System.out.printf("Average of entered numbers: %.2f", average);
+                    break;
+                }
+                average += n;
+                count += 1;
+            }
+
+        } catch (Exception e) {
+            if (e instanceof InputMismatchException) System.out.println("Only numbers are allowed.");
+            else System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        averageCalculator(new Scanner(System.in));
+    }
+}
